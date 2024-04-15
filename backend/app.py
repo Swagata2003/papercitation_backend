370bd5c53cb7b80a3a73ce423f7795f8bbfbe460
Swagata2003 <swagatasinha.pri@gmail.com>
2024-04-15 16:24:18 +0530

    first commit

--- backend/app.py
+++ backend/app.py
@@ -230,5 +230,6 @@ def getpagerank():
     return jsonify(result)
     
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
